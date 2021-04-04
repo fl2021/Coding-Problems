a427c5bf8b3997b7cb2a80c100950670f5745596
fanlu <fanlu913@gmail.com>
2021-04-04 00:19:00 -0400

    Lintcode1454

---
+++ Lintcode/1454 · Word Frequency Count.java
@@ -0,0 +1,45 @@
+public class Solution {
+    /**
+     * @param s: The string s
+     * @param excludeList: The excludeList
+     * @return: Return the most frequent words
+     */
+    public List<String> getWords(String s, String[] excludeList) {
+        // Write your code here
+        Set<String> set = new HashSet<>();
+        for (String str : excludeList) {
+          set.add(str.toLowerCase());
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+          if (Character.isLetter(s.charAt(i))) {
+             int j = i;
+             while (j < s.length() && Character.isLetter(s.charAt(j))) {
+               j++;
+             }
+             String str = s.substring(i, j).toLowerCase();
+             if (!set.contains(str)) {
+               map.put(str, map.getOrDefault(str, 0) + 1);
+             }
+             i = j - 1;
+          }
+        }
+
+        List<String> list = new ArrayList<>();
+        int max = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+          if (entry.getValue() > max) {
+              max = entry.getValue();
+              list.clear();
+              list.add(entry.getKey());
+          } else if (entry.getValue() == max) {
+              list.add(entry.getKey());
+          }
+        }
+
+        Collections.sort(list);
+        return list;
+
+    }
+}
