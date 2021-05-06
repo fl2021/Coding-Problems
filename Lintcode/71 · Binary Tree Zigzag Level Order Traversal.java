ae99462ef9254d3a94abb3f0c0517b15c965d902
fanlu <fanlu913@gmail.com>
2021-05-05 21:18:26 -0400

    Lintcode71

---
+++ Lintcode/71 · Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,50 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: A list of lists of integer include the zigzag level order traversal of its nodes' values.
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        // write your code here
+        List<List<Integer>> results = new ArrayList<>();
+        if (root == null) {
+          return results;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int currentLevel = 1;
+
+        while (!queue.isEmpty()) {
+          List<Integer> temp = new ArrayList<>();
+          int size = queue.size();
+          for (int i = 0; i < size; i++) {
+              TreeNode node = queue.poll();
+              if (results.size() % 2 == 0) {
+                temp.add(node.val);
+              } else {
+                temp.add(0, node.val);
+              }
+              if (node.left != null) {
+                queue.offer(node.left);
+              }
+              if (node.right != null) {
+                queue.offer(node.right);
+              }
+          }
+          results.add(temp);
+        }
+        return results;
+    }
+}
