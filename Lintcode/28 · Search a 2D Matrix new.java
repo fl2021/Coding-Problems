674caaa7184671e34c7a0e33bb3893c29eafdfd3
fanlu <fanlu913@gmail.com>
2021-07-09 14:06:36 -0400

    Lintcode28new

---
+++ Lintcode/28 · Search a 2D Matrix new.java
@@ -0,0 +1,41 @@
+public class Solution {
+    /**
+     * @param matrix: matrix, a list of lists of integers
+     * @param target: An integer
+     * @return: a boolean, indicate whether matrix contains target
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        if (matrix == null || matrix.length == 0) {
+          return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+          return false;
+        }
+
+        int n = matrix.length, m = matrix[0].length;
+        int start = 0, end = n * m - 1;
+        while (start + 1 < end) {
+          int mid = start + (end - start) / 2;
+          if (get(matrix, mid) < target) {
+            start = mid;
+          } else {
+            end = mid;
+          }
+        }
+
+        if (get(matrix, start) == target) {
+          return true;
+        }
+        if (get(matrix, end) == target) {
+          return true;
+        }
+        return false;
+    }
+
+    private int get(int[][] matrix, int index) {
+      int x = index / matrix[0].length;
+      int y = index % matrix[0].length;
+      return matrix[x][y];
+    }
+}
