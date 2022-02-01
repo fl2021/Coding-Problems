082ce189301c2d34bfd7287654254df413ba6c92
fanlu <fanlu913@gmail.com>
2022-02-01 16:06:22 -0500

    some message

---
+++ LeetcodeA/Free/21. Merge Two Sorted Lists.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// 1.recursion, may cause stackoverflow error with some-thousand elements input. // It's nice but impractical.
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+        
+    }
+}
+
+//2.iteration
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node = new ListNode();
+        ListNode head = node;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+        
+        if (list1 != null) {
+            node.next = list1;
+        }
+        if (list2 != null) {
+            node.next = list2;
+        }
+        return head.next;
+        
+    }
+}
