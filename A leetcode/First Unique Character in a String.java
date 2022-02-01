a415983f6aebfb2d6f89ca20158076e223ee9239
fanlu <fanlu913@gmail.com>
2022-02-01 16:52:59 -0500

    some message

---
+++ A leetcode/First Unique Character in a String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+        //create an array to record the 26 characters' times.
+        int[] compare = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            compare[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (compare[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
