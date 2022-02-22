6c4a024d39e44d50198f0d793987ef6a8b9ca8c2
fanlu <fanlu913@gmail.com>
2022-02-21 19:29:32 -0500

    some message

--- A leetcode/9zhang/700. Search in a Binary Search Tree.java
+++ A leetcode/9zhang/700. Search in a Binary Search Tree.java
@@ -16,16 +16,37 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         //recursive
+        //递归三要素之一：递归的出口
         //exit
         if (root == null || root.val == val) {
             return root;
         }
         //split
+        //递归三要素之二：递归的拆解
+        //val在root左子树
         if (root.val < val) {
             return searchBST(root.right, val);
-        } else {
+        } 
+        //val在root右子树
+        else {
             return searchBST(root.left, val);
         }
        
     }
 }
+
+//iteration
+public TreeNode searchBST(TreeNode root, int val) {
+        //如果还可以继续往下找，并且还没找到，就继续找
+        while (root != null || root.val != val) {
+            //val在左子树
+            if (val < root.val) {
+                root = root.left;
+            }
+            //val在右子树
+            else {
+                root = root.right;
+            }
+        }
+        return root;
+    }
