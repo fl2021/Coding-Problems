520df09630e87357ee0804be95cb27d809ed39d2
fanlu <fanlu913@gmail.com>
2022-02-25 00:57:16 -0500

    some message

--- T Leetcode/79. Word Search.java
+++ T Leetcode/79. Word Search.java
@@ -44,3 +44,37 @@ class Solution {
                
     }
 }
+
+class Solution {
+    private int[][] directions = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length, cols = board[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        for(int i = 0; i<rows; i++ ){
+            for(int j = 0; j<cols; j++ ){
+                if (dfs(board, visited, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, boolean[][] visited, String word, int x, int y, int idx){
+        if (idx == word.length()) return true;
+        if (x<0 || x>=board.length||y<0|| y>=board[0].length ||visited[x][y] || board[x][y]!=word.charAt(idx)){
+            return false;
+        }
+        
+        visited[x][y] = true;
+        boolean result = false;
+        for (int[] dir : directions){
+            if (dfs(board, visited, word, x+dir[0], y+dir[1], idx+1)){
+                result = true;
+                break;
+            }
+        }
+        visited[x][y] = false;
+        return result;
+    }
+}
